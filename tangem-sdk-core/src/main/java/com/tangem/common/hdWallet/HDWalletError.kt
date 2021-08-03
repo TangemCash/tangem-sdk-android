85d3ec4c10c0f75053c6352a51a0044b66e9bf82
Anton Zhilenkov <azhilenkov@tangem.com>
2021-08-04 00:31:12 +0300

    CSK-185 implement CKDPub Key

---
+++ tangem-sdk-core/src/main/java/com/tangem/common/hdWallet/HDWalletError.kt
@@ -0,0 +1,10 @@
+package com.tangem.common.hdWallet
+
+/**
+ * Created by Anton Zhilenkov on 03/08/2021.
+ */
+sealed class HDWalletError : Exception() {
+    object HardenedNotSupported : HDWalletError()
+    object DerivationFailed : HDWalletError()
+    object WrongPath : HDWalletError()
+}
